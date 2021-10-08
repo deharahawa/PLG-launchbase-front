ff2b379005dc129083aa81fc1d1b6e09696af3a4
Andre Arahawa <andre.arahawa@ampli.com.br>
2021-10-08 08:58:42 -0300

    feat(front): adjust list buttons

--- src/components/ListButtons/index.tsx
+++ src/components/ListButtons/index.tsx
@@ -1,10 +1,34 @@
+import { useEffect, useState } from "react";
+import { useLaunches } from "../../hooks/useLaunches";
 import { ContainerButton } from "./style";
 
 export function ListButtons() {
+  const { handleLoadData } = useLaunches();
+  const [type, setType] = useState("past");
+
+  useEffect(() => {
+    handleLoadData(type);
+  }, [handleLoadData, type]);
+
+  const buttonTypes = {
+    PAST: "past",
+    UPCOMING: "upcoming",
+  };
+
   return (
     <ContainerButton>
-      <button>Lançamentos passados</button>
-      <button>Lançamentos futuros</button>
+      <button
+        className={type === buttonTypes.PAST ? "isPressed" : "notPressed"}
+        onClick={() => setType(buttonTypes.PAST)}
+      >
+        Lançamentos passados
+      </button>
+      <button
+        className={type === buttonTypes.UPCOMING ? "isPressed" : "notPressed"}
+        onClick={() => setType(buttonTypes.UPCOMING)}
+      >
+        Lançamentos futuros
+      </button>
     </ContainerButton>
   );
 }
