bdfa6734a77e667f0cdec312c093f43076d280b7
Andre Arahawa <andre.arahawa@ampli.com.br>
2021-10-08 08:58:18 -0300

    feat(front): adjust api route

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,5 @@
 import axios from "axios";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 export const api = axios.create({
-  baseURL: process.env.API_URL,
+  baseURL: "http://localhost:3333",
 });
