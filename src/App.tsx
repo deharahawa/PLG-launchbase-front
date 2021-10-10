e717a5af66f93df2e066e7dcbe002d36fd56f575
Andre Arahawa <andre.arahawa@ampli.com.br>
2021-10-10 20:10:58 -0300

    feat(frontend): configure modal root

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+import Modal from "react-modal";
+
+Modal.setAppElement("#root");
 
 function App() {
   return (
