039839a708db73e19a030d97cf69092ea4dd1591
Andre Arahawa <andre.arahawa@ampli.com.br>
2021-10-09 21:13:34 -0300

    feat(frontend): adjust api base url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL:
-    process.env.API_BASE_URL || "https://plg-launchbase-backend.herokuapp.com",
+  baseURL: process.env.API_BASE_URL,
 });
